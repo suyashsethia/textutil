652a2aab7449c6a9844f5ecb80cf81d5c9a93fab
suyashsethia <suyash.sethia0848@gmail.com>
2022-10-31 22:07:50 +0530

    added 3 buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <>
       <Navbar title="textutil"/>
       <div className="container" my-3>
-      <TextForm/>
+      <TextForm heading1="Enter your text here" heading2="and here is your output ..." button1="ALL CAPITAL" button2 = "all small"/>
       </div>
     </>
   );
