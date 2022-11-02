53cd39153f2f44431feb965ea98d72f9c8db8064
suyashsethia <suyash.sethia0848@gmail.com>
2022-11-02 10:26:55 +0530

    modified Textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 export default function TextForm(props) {
 
     const func_onchange = (event) => {
@@ -25,26 +26,24 @@ export default function TextForm(props) {
         var out_mockery = "";
         var k = 1;
         var list = text.split(" ");
-        for (let index = 0; index < list.length; index++) 
-        {
+        for (let index = 0; index < list.length; index++) {
             // const element = list[index];
-            for (let index2 = 0; index2 < list[index].length; index2++) 
-            {
-                var j="";
+            for (let index2 = 0; index2 < list[index].length; index2++) {
+                var j = "";
                 // const element2 = list[index][index2];
                 if (k === 1) {
                     j = list[index][index2].toUpperCase();
                     k = 0;
-                    out_mockery+=j ;
+                    out_mockery += j;
                 }
                 else {
                     j = list[index][index2].toLowerCase();
                     k = 1;
-                    out_mockery+=j ;
+                    out_mockery += j;
 
                 }
             }
-            out_mockery +=" ";
+            out_mockery += " ";
         }
         setOutput(out_mockery);
 
@@ -70,6 +69,9 @@ export default function TextForm(props) {
             <button type="submit" onClick={Tolowercase} className="btn btn-outline-primary mx-1  ">{props.button2}</button>
 
             <button type="submit" onClick={Tomockery} className="btn btn-outline-primary mx-1 ">Mockery</button>
+
+            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            </div>
             <div className="mb-2 mt-3" >
                 <label htmlFor="output_box" className="form-label"><h3>{props.heading2}</h3></label>
                 <textarea className="form-control" id="output_box" value={output} aria-describedby="emailHelp" rows="6" />
